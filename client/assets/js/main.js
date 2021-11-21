b996c9c7e41d2a4ae8309778f09e2433370e0f98
bethanvaughan <bpvaughan97@gmail.com>
2021-11-21 16:30:16 +0200

    deleted other unnecessary files and folders from css template

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,5 +1,18 @@
-$('.edica-header .dropdown').hover(function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-}, function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
-});
+$(".edica-header .dropdown").hover(
+  function () {
+    $(this)
+      .find(".dropdown-menu")
+      .first()
+      .stop(true, true)
+      .delay(250)
+      .slideDown();
+  },
+  function () {
+    $(this)
+      .find(".dropdown-menu")
+      .first()
+      .stop(true, true)
+      .delay(100)
+      .slideUp();
+  }
+);
